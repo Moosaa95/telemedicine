6addc77103735b1b195c4ce9403572bed68b850c
moosa <moosaabdullahi45@gmail.com>
2024-08-05 11:13:48 +0100

    new

---
+++ app/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+// import NextAuth from "next-auth"
+// import EmailProvider from "next-auth/providers/email"
+// import GoogleProvider from "next-auth/providers/google"
+
+
+// if (!process.env.EMAIL_SERVER || !process.env.EMAIL_FROM || !process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+//     throw new Error("Missing environment variables for authentication");
+// }
+
+
+// export default NextAuth({
+//     providers: [
+//         EmailProvider({
+//             server: process.env.EMAIL_SERVER!,
+//             from: process.env.EMAIL_FROM!
+//         }),
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_CLIENT_ID!,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+//         }),
+//     ]
+// })
+
